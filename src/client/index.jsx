a6dc1be1e1e62fa508ebdf14e44303d57b7d3ccc
Maxym <maxymbachynskyi@gmail.com>
2017-07-15 19:41:06 +0300

    08 - Bootstrap and JSS

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -12,11 +12,17 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 
 import helloReducer from '../shared/reducer/hello'
+import { APP_CONTAINER_SELECTOR, JSS_SSR_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 
 window.jQuery = $
 window.Tether = Tether
 require('bootstrap')
+
+// use only for client side apps
+// const jssServerSide = document.querySelector(JSS_SSR_SELECTOR)
+// // flow-disable-next-line
+// jssServerSide.parentNode.removeChild(jssServerSide)
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
@@ -27,5 +33,6 @@ const store = createStore(combineReducers(
   { hello: helloReducer }),
   { hello: Immutable.fromJS(preloadedState.hello) },
   composeEnhancers(applyMiddleware(thunkMiddleware)));
+
 
 setUpSocket(store)
