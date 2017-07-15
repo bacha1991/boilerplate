aed8df6bbba3bb2217b5cfbf7f0988896e94f999
Maxym <maxymbachynskyi@gmail.com>
2017-07-15 18:17:35 +0300

    07 - Socket.IO

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -3,18 +3,12 @@
 import 'babel-polyfill'
 
 import Immutable from 'immutable'
+import setUpSocket from './socket'
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-import { Provider } from 'react-redux'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import { BrowserRouter } from 'react-router-dom'
 
-import App from '../shared/app'
 import helloReducer from '../shared/reducer/hello'
-import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 
 /* eslint-disable no-underscore-dangle */
@@ -25,4 +19,6 @@ const preloadedState = window.__PRELOADED_STATE__
 const store = createStore(combineReducers(
   { hello: helloReducer }),
   { hello: Immutable.fromJS(preloadedState.hello) },
-  composeEnhancers(applyMiddleware(thunkMiddleware)))
+  composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+setUpSocket(store)
